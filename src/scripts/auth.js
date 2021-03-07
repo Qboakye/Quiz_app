135b8a671e705d9ec280e0eee4c509f5fa4741dc
qboakye <qboakye@gmail.com>
2021-03-08 07:41:13 +0800

    added auth for sign up and log in

---
+++ src/scripts/auth.js
@@ -0,0 +1,29 @@
+/*Selectors */
+let signupForm = document.querySelector("#signup-form")
+let loginForm = document.querySelector("#login-form")
+
+/* EventListeners */
+signupForm.addEventListener("submit", () => {
+    let email = signupForm["email"].value
+    let password = signupForm["password"].value
+    console.log(email, password)
+    auth.createUserWithEmailAndPassword(email, password).then(()=> {
+        console.log("created")
+        signupCloseBtn.addEventListener("click", () => signup.classList.add("visible"))
+    })
+})
+
+loginForm.addEventListener("submit", () => {
+    let email = loginForm["email"].value
+    let password = loginForm["password"].value
+    console.log(email, password)
+    auth.signInWithEmailAndPassword(email, password).then(()=> {
+
+        let newString = window.location.toString()
+        let location = newString.slice(0, newString.lastIndexOf("/"))
+        window.location.assign(location + "/paths/student.html")
+
+        loginCloseBtn.addEventListener("click", () => login.classList.add("visible"))
+    })
+})
+/*Functions */
