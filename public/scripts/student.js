11846f04e10ef72dd4c48e9ef2f62ce5ce3873b0
qboakye <qboakye@gmail.com>
2021-03-12 01:33:30 +0800

    reworked the radio buttons to prevent multiple selection

--- public/scripts/student.js
+++ public/scripts/student.js
@@ -18,10 +18,10 @@ function questions(){
     studentArea.innerHTML = `
         <p class="question"> <span class="">${num + 1}.</span> ${data.question}</p>
         <div class="possible-ans">
-            <label><input type="radio" name="a" value="${data.a}" id="a">${data.a}</label>
-            <label><input type="radio" name="b" value="${data.b}" id="b">${data.b}</label>
-            <label><input type="radio" name="c" value="${data.c}" id="c">${data.c}</label>
-            <label><input type="radio" name="d" value="${data.d}" id="d">${data.d}</label>	
+            <label><input type="radio" name="studen-ques" value="${data.a}" id="a">${data.a}</label>
+            <label><input type="radio" name="studen-ques" value="${data.b}" id="b">${data.b}</label>
+            <label><input type="radio" name="studen-ques" value="${data.c}" id="c">${data.c}</label>
+            <label><input type="radio" name="studen-ques" value="${data.d}" id="d">${data.d}</label>	
         </div>
         
         <button class="submit" type="Submit">Submit</button>
@@ -34,10 +34,10 @@ studentArea.addEventListener("submit", () => {
         auth.onAuthStateChanged(user => {
             if(user){ 
                 const{a, b, c, d} = studentArea
-                let answerA = a.checked && a.name
-                let answerB = b.checked && b.name
-                let answerC = c.checked && c.name
-                let answerD = d.checked && d.name
+                let answerA = a.checked && a.id
+                let answerB = b.checked && b.id
+                let answerC = c.checked && c.id
+                let answerD = d.checked && d.id
 
                 let answered = answerA || answerB || answerC || answerD
                 
